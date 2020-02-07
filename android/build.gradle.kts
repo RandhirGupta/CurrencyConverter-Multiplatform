782b4c06622361f7f1a3ef9912f2eb89040b7685
Cyborg <randhirgupta92@gmail.com>
2020-02-07 12:54:12 +0530

    Chore : Build config changes

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -22,7 +22,13 @@ android {
     }
 
     buildTypes {
+
         getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+
+        getByName("debug") {
             isMinifyEnabled = true
             proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
         }
